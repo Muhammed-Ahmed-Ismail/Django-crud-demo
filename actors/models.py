6be426408677bd32853512c664b82427f4dc0384
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-06-05 15:14:33 +0200

    adding actors crud

--- actors/models.py
+++ actors/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 #from movies.models import Movie
 
-GENDER_LIST = [('m', 'male'), ('f', 'female')]
+GENDER_LIST = [('male', 'male'), ('female', 'female')]
 
 
 # Create your models here.
 class Actor(models.Model):
-    name = models.fields.CharField(verbose_name='movie', max_length=25, unique=True)
+    name = models.fields.CharField(verbose_name='Name', max_length=25, unique=True)
     gender = models.CharField(verbose_name='Gender', choices=GENDER_LIST, max_length=6,default='male')
     age = models.IntegerField(default=0)
     # movies = models.ManyToManyField('movies.movie')
